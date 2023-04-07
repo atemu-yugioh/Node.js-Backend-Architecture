a6f1b70ba9a99ae53f4950139e480bbee085dc75
Nguyen Huu Thieng <16521532@gm.uit.edu.vn>
2023-04-07 08:50:32 +0700

    #6 Custom Dynamic Middlwware For ApiKey And Permission

---
+++ src/models/apiKey.model.js
@@ -0,0 +1,34 @@
+const { model, Schema, Types } = require("mongoose");
+
+const DOCUMENT_NAME = "ApiKey";
+const COLLECTION_NAME = "Apikeys";
+
+const apiKeySchema = new Schema(
+  {
+    key: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+    permissions: {
+      type: [String],
+      required: true,
+      enum: ["0000", "1111", "2222"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: "30d",
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+module.exports = model(DOCUMENT_NAME, apiKeySchema);
